92d4c097550068e41791b29b47b6d9882e5c81ce
Bing <i@hebing.me>
2020-08-29 14:54:37 +0800

    Initial commit

---
+++ src/me/hebing/flappy/Main.java
@@ -0,0 +1,136 @@
+package me.hebing.flappy;
+
+import me.hebing.flappy.graphics.Shader;
+import me.hebing.flappy.input.Input;
+import me.hebing.flappy.level.Level;
+import me.hebing.flappy.math.Matrix4f;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.GL_TEXTURE1;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Main {
+
+    private int width = 1280;
+    private int height = 720;
+
+    private boolean running = true ;
+
+    private long window;
+
+    private Level level;
+
+    private void init() {
+        if (!glfwInit()) {
+            System.err.println("Could not initialize GLFW!");
+            return;
+        }
+
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        window = glfwCreateWindow(width, height, "Flappy", NULL, NULL);
+        if (window == NULL) {
+            System.err.println("Could not create GLFW window!");
+            return;
+        }
+
+        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+        // Center the window
+        glfwSetWindowPos(
+                window,
+                (vidmode.width() - width) / 2,
+                (vidmode.height() - height) / 2
+        );
+        glfwSetKeyCallback(window, new Input());
+
+        glfwMakeContextCurrent(window);
+        glfwShowWindow(window);
+        GL.createCapabilities();
+
+        glEnable(GL_DEPTH_TEST);
+        glActiveTexture(GL_TEXTURE1);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        System.out.println("OpenGL: " + glGetString(GL_VERSION));
+        Shader.loadAll();
+
+        Matrix4f pr_matrix = Matrix4f.orthographic(-10.0f, 10.0f, -10.0f * 9.0f / 16.0f, 10.0f * 9.0f / 16.0f, -1.0f, 1.0f);
+        Shader.BG.setUniformMat4f("pr_matrix", pr_matrix);
+        Shader.BG.setUniform1i("tex", 1);
+
+        Shader.BIRD.setUniformMat4f("pr_matrix", pr_matrix);
+        Shader.BIRD.setUniform1i("tex", 1);
+
+        Shader.PIPE.setUniformMat4f("pr_matrix", pr_matrix);
+        Shader.PIPE.setUniform1i("tex", 1);
+
+        level = new Level();
+    }
+
+    public void run() {
+        init();
+
+        long lastTime = System.nanoTime();
+        double delta = 0.0;
+        double ns = 1000000000.0 / 60.0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1.0) {
+                update();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println(updates + " ups, " + frames + " fps");
+                updates = 0;
+                frames = 0;
+            }
+            if (glfwWindowShouldClose(window))
+                running = false;
+        }
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    private void update() {
+        glfwPollEvents();
+        level.update();
+        if (level.isGameOver()) {
+            level = new Level();
+        }
+    }
+
+    private void render() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        level.render();
+
+        int error = glGetError();
+        if (error != GL_NO_ERROR)
+            System.out.println(error);
+
+        glfwSwapBuffers(window);
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+}
---
+++ src/me/hebing/flappy/input/Input.java
@@ -0,0 +1,18 @@
+package me.hebing.flappy.input;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWKeyCallback;
+
+public class Input extends GLFWKeyCallback {
+
+    public static boolean[] keys = new boolean[65536];
+
+    public static boolean isKeyDown(int keycode) {
+        return keys[keycode];
+    }
+
+    public void invoke(long window, int key, int scancode, int action, int mods) {
+        keys[key] = action != GLFW.GLFW_RELEASE;
+    }
+
+}
